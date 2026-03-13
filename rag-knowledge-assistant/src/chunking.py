d30ead386a67c835c4a7a050254f06cc583ec78b
Pooja <pkashyap@lululemon.com>
2026-03-13 12:33:11 +0530

    chunking.py added

---
+++ rag-knowledge-assistant/src/chunking.py
@@ -0,0 +1,21 @@
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from loader import load_documents
+
+def split_documents(documents):
+
+    splitter = RecursiveCharacterTextSplitter(
+        chunk_size=500,
+        chunk_overlap=100
+    )
+
+    chunks = splitter.split_documents(documents)
+
+    return chunks
+
+docs = load_documents("C:\\Users\\pkashyap\\pooja-919Github\\ai-portfolio\\rag-knowledge-assistant\\data\\documents")
+chunks = split_documents(docs)
+
+print("Number of chunks:", len(chunks))
+print(chunks[0].page_content)
+print(".......")
+print(chunks[1].page_content)
